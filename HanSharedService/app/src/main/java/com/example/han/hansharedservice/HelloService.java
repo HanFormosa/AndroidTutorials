ded33b02dd772cd663c52fce2fa7f8148b9a52ea
HanPersonal <tzehan_su@hotmail.com>
2016-06-29 17:34:14 +0800

    added internet permission. added socket service stuff, not sure code not working or because network issue. seems like iOS can;t connect as well.

--- HanSharedService/app/src/main/java/com/example/han/hansharedservice/HelloService.java
+++ HanSharedService/app/src/main/java/com/example/han/hansharedservice/HelloService.java
@@ -4,14 +4,31 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
+import android.util.Log;
 import android.widget.Toast;
 
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+
 public class HelloService extends Service {
+    private static final String TAG = "SharedService";
+
     public HelloService() {
     }
 
+    String SERVERIP = "192.168.2.107";
+    public static final int SERVERPORT = 5004;
 
     private static final String tag = HelloService.class.getSimpleName();
+
+    PrintWriter out;
+    Socket socket;
+    InetAddress serverAddr;
+    DataInputStream is;
 
     // Keep track of the services run state.
     private boolean run = false;
@@ -30,21 +47,33 @@ public class HelloService extends Service {
     private IPostListener _callback = null;
 
     // Our thread that counts the run time.
-    private Thread thread;
+    private Thread connect;
+//    private Runnable connect;
 
     @Override
     public IBinder onBind(Intent intent) {
         return _binder;
     }
 
+    public void sendMessage(String message){
+        if (out != null && !out.checkError()) {
+            Log.i(TAG,"in sendMessage: "+message);
+//            setCmd(message);
+//            Log.i(TAG,"this is result from callback func : " + serviceCallbacks.processDataReceivedfromDevice());
+            out.println(message);
+            out.flush();
+        }
+    }
     @Override
     public int onStartCommand(Intent intent, int flags, int startId)
     {
         Toast.makeText(this, "service starting", Toast.LENGTH_SHORT).show();
 
         // Start the thread when the service is started.
-        startThread();
+//        startThread();
 
+
+        startThread();
         // If we get killed, after returning from here, restart
         return START_STICKY;
     }
@@ -53,6 +82,13 @@ public class HelloService extends Service {
     public void onDestroy()
     {
         Toast.makeText(this, "service done", Toast.LENGTH_SHORT).show();
+        try {
+            socket.close();
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        socket = null;
 
         // Stop the thread when the service is destroyed.
         stopThread();
@@ -66,10 +102,10 @@ public class HelloService extends Service {
         if (!run)
         {
             run = true;
-
             // Start up the thread running the service.
-            thread = new Thread(new WorkThread());
-            thread.start();
+            connect = new Thread(new connectSocket());
+            connect.start();
+//            new Thread(connect).start();
         }
     }
 
@@ -80,10 +116,10 @@ public class HelloService extends Service {
     {
         run = false;
 
-        if (thread != null)
+        if (connect != null)
         {
-            thread.interrupt();
-            thread = null;
+            connect.interrupt();
+            connect = null;
         }
     }
 
@@ -106,6 +142,71 @@ public class HelloService extends Service {
             _callback = callback;
         }
     }
+
+    class connectSocket implements Runnable {
+//        String result = null;
+//        byte[] data = new byte[64];
+        @Override
+        public void run() {
+            Log.i(TAG,"i am in run of connectSocket started");
+//            mRun = true;
+            try {
+                //here you must put your computer's IP address.
+                serverAddr = InetAddress.getByName(SERVERIP);
+                Log.i(TAG, "C: Connecting... at address :" + SERVERIP + " " + serverAddr);
+                //create a socket to make the connection with the server
+
+                socket = new Socket(serverAddr, SERVERPORT);
+//                Log.i(TAG, "mRun stat is " + mRun);
+                try {
+
+                    //send the message to the server
+                    out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
+                    //Get the input stream of the client socket TODO: keep putting messages into "in" a buffered reader.
+//                    in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+//                    is = new DataInputStream(socket.getInputStream());
+//                    Log.i(TAG, "input stream is ran");
+//                    while (mRun) { //TODO: keep running, unless want to stop listening, call stopClient()?
+////                        serverMessage = in.readLine();
+////
+//                        String[] s = new String[64];
+//                        int size = is.read(data);
+////                        Log.i(TAG, "data is  " + Arrays.toString(data) + "size is: " + size);
+////                        for(int i = 0 ; i < size; i ++){
+////                            s[i] = String.format("%02X",data[i]);
+////                            Log.i(TAG, "data is  " + s[i] + "pos is: " + i);
+////                        }
+//
+//                        //TODO: call a method in activity to pass the message to bounded activity (maybe by service callbacks)
+//                        //only call method if data is not empty , detect empty bytes, detect CRLF byte
+//                        if (size != 0){
+//                            //detect CRLF byte
+//                            String s63 = String.format("%02X", data[62]);//"\r"
+//                            String s64 = String.format("%02X", data[63]);//"\n"
+//                            if (s63.equals("0D") && s64.equals("0A")){
+//                                //TODO: update the cmd available
+//                                if (cmd.equals("E3") || cmd.equals("68") || cmd.equals("ED") || cmd.equals("EC")){
+//                                    //call callback
+//                                    Log.i(TAG, "callback will be called");
+//                                    serviceCallbacks.processDataReceivedfromDevice(data);
+//                                }
+//                            }
+//                        }
+//
+//                    }
+                    Log.e("TCP Client", "C: Sent.");
+                    Log.e("TCP Client", "C: Done.");
+//                    Log.i(TAG,"REsult is " + result);
+                }
+                catch (Exception e) {
+                    Log.e("TCP", "S: Error", e);
+                }
+            } catch (Exception e) {
+                Log.e("TCP", "C: Error", e);
+            }
+        }
+    }
+
 
     /**
      * The Runnable class for our thread that counts the run time.

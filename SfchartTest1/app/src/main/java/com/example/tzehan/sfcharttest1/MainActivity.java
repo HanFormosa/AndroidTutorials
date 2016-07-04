700d9b0447c0b94dcd3a17d01c1c6c9cb15561e5
HanPerGmail <tzehan.su@gmail.com>
2016-07-04 21:30:20 +0800

    mistake comment

--- SfchartTest1/app/src/main/java/com/example/tzehan/sfcharttest1/MainActivity.java
+++ SfchartTest1/app/src/main/java/com/example/tzehan/sfcharttest1/MainActivity.java
@@ -10,9 +10,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);\
+        setContentView(R.layout.activity_main);
 
 
         SfChart sfChart = new SfChart(this);
+
     }
 }
